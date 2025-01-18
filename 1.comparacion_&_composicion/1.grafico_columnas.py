d531805fa36fdc016dc06f3492b9368243f490ab
K G B <kargb@ciencias.unam.mx>
2025-01-18 14:09:57 -0600

    grafica de barras

--- 1.comparacion_&_composicion/1.grafico_columnas.py
+++ 1.comparacion_&_composicion/1.grafico_columnas.py
@@ -1,2 +1,14 @@
 # Usaremos Spyder
+import pandas as pd
+import matplotlib.pyplot as plt
 
+dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+
+tempe_max = [65, 54, 32, 35, 44, 40, 58]
+df = pd.DataFrame(dias, tempe_max)
+
+plt.bar(dias, tempe_max)
+plt.title('Gráfica de barras')
+plt.xlabel('Días')
+plt.ylabel('Temperatura máxima')
+plt.show()
