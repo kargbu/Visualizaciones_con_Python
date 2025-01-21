328c46e471999678407694e872f89a90ec385c36
kargbu <kargb@ciencias.unam.mx>
2025-01-21 14:54:40 -0600

    gráficas de disperción

--- Visualización_Python/4.gráfico_dispercion.py
+++ Visualización_Python/4.gráfico_dispercion.py
@@ -1,3 +1,16 @@
 import matplotlib.pyplot as plt
 import statistics as sta
-import pandas as pd 
+import pandas as pd
+
+# Esta es la sintaxis básica plt.scatter(x, y)
+
+import random
+x = random.sample(range(100),20)
+y = random.sample(range(100),20)
+print(x)
+print(y)
+
+x = [66, 72, 5, 91, 46, 70, 65, 96, 68, 17, 51, 83, 13, 8, 45, 63, 21, 11, 97, 54]
+y = [72, 54, 55, 2, 29, 41, 90, 30, 7, 33, 47, 39, 63, 58, 73, 85, 66, 11, 14, 4]
+plt.scatter(x, y)
+plt.show()
