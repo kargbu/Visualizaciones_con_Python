3b4ec9ff5e2f87e75b124c021a3308cc027a0d27
kargbu <kargb@ciencias.unam.mx>
2025-01-30 15:19:23 -0600

    ejercicios

---
+++ Visualización_Python/ejercicio_1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+
+x = [17, 72, 45, 86, 96, 24, 27, 32, 38, 94, 70]
+y = [78, 35, 28, 75, 2, 22, 25, 41, 73, 51, 58]
+
+plt.scatter(x, y)
+plt.title('Números aleatorios')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+
+'''
+Etiqueta el gráfico:
+Añade la etiqueta «Números aleatorios» al gráfico de dispersión.
+Guarde su código:
+Asegúrate de guardar tu código en el archivo temp.py después de hacer las modificaciones.
+'''
